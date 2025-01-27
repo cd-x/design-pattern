fa5af87b4513216073e5222a266ebca9b41f616c
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-01-27 20:46:36 +0530

    other tests

--- src/test/java/patterns/StatePatternTest.java
+++ src/test/java/patterns/StatePatternTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StatePatternTest {
     @Test
     public void test_gumball_when_filled(){
@@ -15,5 +17,27 @@ public class StatePatternTest {
             gumballMachine.turnCrank();
             gumballMachine.dispense();
         });
+    }
+    @Test
+    void test_refill_when_serving_gumball(){
+        IGumballMachine gumballMachine = new GumballMachine(2);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.refill("123", 5);
+    }
+    @Test
+    void test_eject_when_quarter_inserted(){
+        IGumballMachine gumballMachine = new GumballMachine(3);
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+    }
+    @Test
+    void test_refill_when_no_quarter(){
+        GumballMachine gumballMachine = new GumballMachine(0);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.dispense();
+        gumballMachine.refill("123", 7);
+        assertEquals(7, gumballMachine.getCount());
     }
 }
